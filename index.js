20a16e0afc0452f87e9a65cf91e9f09ed9778d4f
Daniel Calvo <dcalvom@gmail.com>
2017-04-25 18:44:05 -0600

    Change concatenation of text

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ restService.use(bodyParser.json());
 restService.post('/tutoria', function(req, res) {
     var respuesta = ""
     if(req.body.result && req.body.result.parameters){     
-      var nombre = req.body.result.parameters.nombre ? req.body.result.parameters.nombre : ""
-      var introduccion = req.body.result.parameters.introduccion ? req.body.result.parameters.introduccion : ""
-      var gustos = req.body.result.parameters.gustos ? req.body.result.parameters.gustos : ""
-      var caracter = req.body.result.parameters.caracter ? req.body.result.parameters.caracter : ""
-      var otros_gustos = req.body.result.parameters.otros_gustos ? req.body.result.parameters.otros_gustos : ""
-      var modelo = req.body.result.parameters.modelo ? req.body.result.parameters.modelo : ""
-      var decision_computacion = req.body.result.parameters.decision_computacion ? req.body.result.parameters.decision_computacion : ""
-      var curso_favorito = req.body.result.parameters.curso_favorito ? req.body.result.parameters.curso_favorito : ""
-      respuesta = respuesta.concat(introduccion,"\n",gustos,"\n",caracter,"\n",otros_gustos,"\n",modelo,"\n",decision_computacion,"\n",curso_favorito)
+      var nombre = req.body.result.parameters.nombre ? req.body.result.parameters.nombre.concat(".\n") : ""
+      var introduccion = req.body.result.parameters.introduccion ? req.body.result.parameters.introduccion.concat(".\n") : ""
+      var gustos = req.body.result.parameters.gustos ? req.body.result.parameters.gustos.concat(".\n") : ""
+      var caracter = req.body.result.parameters.caracter ? req.body.result.parameters.caracter.concat(".\n") : ""
+      var otros_gustos = req.body.result.parameters.otros_gustos ? req.body.result.parameters.otros_gustos.concat(".\n") : ""
+      var modelo = req.body.result.parameters.modelo ? req.body.result.parameters.modelo.concat(".\n") : ""
+      var decision_computacion = req.body.result.parameters.decision_computacion ? req.body.result.parameters.decision_computacion.concat(".\n") : ""
+      var curso_favorito = req.body.result.parameters.curso_favorito ? req.body.result.parameters.curso_favorito.concat(".\n") : ""
+      respuesta = respuesta.concat(introduccion,gustos,caracter,otros_gustos,modelo,decision_computacion,curso_favorito)
     }
     else{
       respuesta = "No te he podido escuchar bien. Me podés contar de nuevo."
