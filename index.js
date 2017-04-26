395f9609c4021e625de9e4e40af3b0eadf7f0231
Daniel Calvo <dcalvom@gmail.com>
2017-04-25 18:39:47 -0600

    Changed value for each parameter of the request

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ restService.use(bodyParser.json());
 restService.post('/tutoria', function(req, res) {
     var respuesta = ""
     if(req.body.result && req.body.result.parameters){     
-      var nombre = req.body.result.parameters.echoText ? req.body.result.parameters.echoText : ""
-      var introduccion = req.body.result.parameters.echoText ? req.body.result.parameters.echoText : ""
-      var gustos = req.body.result.parameters.echoText ? req.body.result.parameters.echoText : ""
-      var caracter = req.body.result.parameters.echoText ? req.body.result.parameters.echoText : ""
-      var otros_gustos = req.body.result.parameters.echoText ? req.body.result.parameters.echoText : ""
-      var modelo = req.body.result.parameters.echoText ? req.body.result.parameters.echoText : ""
-      var decision_computacion = req.body.result.parameters.echoText ? req.body.result.parameters.echoText : ""
-      var curso_favorito = req.body.result.parameters.echoText ? req.body.result.parameters.echoText : ""
+      var nombre = req.body.result.parameters.nombre ? req.body.result.parameters.nombre : ""
+      var introduccion = req.body.result.parameters.introduccion ? req.body.result.parameters.introduccion : ""
+      var gustos = req.body.result.parameters.gustos ? req.body.result.parameters.gustos : ""
+      var caracter = req.body.result.parameters.caracter ? req.body.result.parameters.caracter : ""
+      var otros_gustos = req.body.result.parameters.otros_gustos ? req.body.result.parameters.otros_gustos : ""
+      var modelo = req.body.result.parameters.modelo ? req.body.result.parameters.modelo : ""
+      var decision_computacion = req.body.result.parameters.decision_computacion ? req.body.result.parameters.decision_computacion : ""
+      var curso_favorito = req.body.result.parameters.curso_favorito ? req.body.result.parameters.curso_favorito : ""
       respuesta = respuesta.concat(introduccion,"\n",gustos,"\n",caracter,"\n",otros_gustos,"\n",modelo,"\n",decision_computacion,"\n",curso_favorito)
     }
     else{
